e7d3d2a75f5672e9957b0ecd2833984558583add
Artem <southred@yandex.ru>
2018-05-02 15:37:20 +0300

    clean up more

--- src/Main.hs
+++ src/Main.hs
@@ -9,19 +9,17 @@ import Network.HTTP.Types
 import Network.Wai
 import Network.Wai.Handler.Warp (run)
 
+main :: IO ()
+main = do
+  putStrLn "http://localhost:8080/"
+  run 8080 app
+
 app :: Application
 app request respond =
   respond $
-  case (pathInfo request) of
-    ["hello"] ->
-      responseLBS status200 [("Content-Type", "text/plain")] "Hello!"
-    other ->
-      responseLBS status200 [("Content-Type", "text/plain")] "11111"
+  case pathInfo request of
+    ["hello"] -> responseLBS status200 [("Content-Type", "text/plain")] "Hello!"
+    other -> responseLBS status200 [("Content-Type", "text/plain")] "11111"
   where
     make404 :: B.ByteString -> BL.ByteString
     make404 route = BCL.concat [BCL.pack "Not found ", BL.fromStrict route]
-
-main :: IO ()
-main = do
-  putStrLn $ "http://localhost:8080/"
-  run 8080 app
