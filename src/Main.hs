9598be655f1a0e795d5796e7def609f74801acc7
Artem <southred@yandex.ru>
2018-05-16 22:50:28 +0300

    servant hello world

--- src/Main.hs
+++ src/Main.hs
@@ -1,62 +1,42 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
 import Data.Aeson
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Lazy as BL
-import qualified Data.ByteString.Lazy.Char8 as BCL
-import Data.IORef (IORef, atomicModifyIORef', newIORef)
+
 import GHC.Generics
-import Network.HTTP.Types
 import Network.Wai
-import Network.Wai.Handler.Warp (run)
-
-main :: IO ()
-main = do
-  counter <- newIORef 0
-  putStrLn "http://localhost:8080/"
-  run 8080 (app counter)
-
-app :: IORef Int -> Application
-app counter request respond =
-  case pathInfo request of
-    ["hello"] -> do
-      current <-
-        atomicModifyIORef'
-          counter
-          (\c ->
-             let x = c + 1
-             in (x, x))
-      respond $
-        responseLBS
-          status200
-          [("Content-Type", "text/plain")]
-          (BCL.pack ("Hello! You are the " ++ (show current ++ " one")))
-    ["users"] ->
-      case parseMethod $ requestMethod request of
-        (Right GET) -> do
-          let user = User 1 "test@email.com"
-              json = encode user
-          respond $
-            responseLBS status200 [("Content-Type", "application/json")] json
-        (Right POST) -> do
-          b <- requestBody request
-          print b
-          respond $
-            responseLBS status200 [("Content-Type", "application/json")] "ssss"
-    other ->
-      respond $ responseLBS status200 [("Content-Type", "text/plain")] "11111"
-  where
-    make404 :: B.ByteString -> BL.ByteString
-    make404 route = BCL.concat [BCL.pack "Not found ", BL.fromStrict route]
+import Network.Wai.Handler.Warp
+import Servant
 
 data User = User
   { id :: Int
   , email :: String
-  } deriving (Show, Generic)
+  , firstName :: String
+  , lastName :: String
+  , gender :: String
+  , birthDate :: String
+  } deriving (Eq, Show, Generic)
+
+instance FromJSON User
 
 instance ToJSON User
 
-instance FromJSON User
+type API = "users" :> Get '[ JSON] User
+
+startApp :: IO ()
+startApp = run 8080 app
+
+app :: Application
+app = serve api server
+
+api :: Proxy API
+api = Proxy
+
+server :: Server API
+server = return $ User 1 "example@mail.ru" "John" "Doe" "m" "1613433600"
+
+main = startApp
